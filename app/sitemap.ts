90dba89caacdbdc2c631370fb04c45e32b1afc70
sharmadevendra1 <47150042+sharmadevendra1@users.noreply.github.com>
2026-08-26 00:02:21 +0530

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from 'next'
+export default function sitemap(): MetadataRoute.Sitemap { return [{url:'https://go-health-ready.vercel.app/'},{url:'https://go-health-ready.vercel.app/schemes'}] }
